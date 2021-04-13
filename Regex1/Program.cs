4b04c40fd4364b91a6f8d3d59b2b3ba5cb25831e
Copying-Ninja <2001vlad2908@gmail.com>
2021-04-13 04:44:23 +0300

    Add project files.

---
+++ Regex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace Regex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Lorem ipsum chu sho.";
+            var output = Regex.Replace(str.Split()[0], @"[^0-9a-zA-Z\ ]+", "");
+            var output1 = Regex.Replace(str.Split().Last(), @"[^0-9a-zA-Z\ ]+", "");
+            Console.WriteLine(output + " " + output1);
+            Console.ReadKey();
+        }
+    }
+}
